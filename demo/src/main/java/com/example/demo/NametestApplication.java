7d3075826fb6678b97ebbb2181884c5b42480f21
g <g@gonoMacBook-Air.local>
2020-11-19 22:37:46 +0900

    init

---
+++ demo/src/main/java/com/example/demo/NametestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NametestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NametestApplication.class, args);
+	}
+
+}
